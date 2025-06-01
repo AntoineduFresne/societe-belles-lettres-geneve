4d6e26881c9c82eb03b10faa8d285a79093b6ef3
AntoineduFresne <75640413+AntoineduFresne@users.noreply.github.com>
2025-06-01 22:13:46 +0200

    Delete societe-belles-lettres directory

--- societe-belles-lettres/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script pour lancer l'application Flask
-cd /home/ubuntu/societe-belles-lettres-flask
-export FLASK_APP=src/main.py
-export FLASK_ENV=development
-python3 -m flask run --host=0.0.0.0
